519b701511c123d6af605d0ea809f922e9be4e92
Gambitier <akash.jadhav.cse@gmail.com>
2025-06-04 01:37:28 +0530

    fix: update timestamp formatting in useWatchlistSubscription to use locale-specific string representation

--- lib/hooks/useWatchlistSubscription.ts
+++ lib/hooks/useWatchlistSubscription.ts
@@ -159,7 +159,15 @@ export function useWatchlistSubscription() {
                                 volume: VolumeTradedToday,
                                 timestamp: new Date(
                                     quote.ExchangeFeedTimeEpochMillis
-                                ).toISOString(),
+                                ).toLocaleString("en-US", {
+                                    month: "2-digit",
+                                    day: "2-digit",
+                                    year: "numeric",
+                                    hour: "2-digit",
+                                    minute: "2-digit",
+                                    second: "2-digit",
+                                    hour12: false,
+                                }),
                             };
 
                             if (existingIndex >= 0) {
